798b198fb5091ce77ec19c2495da50af96e77a43
sunita0987 <sunita0987546@gmail.com>
2025-01-23 09:23:37 +0530

    first commit

--- assets/js/basic.js
+++ assets/js/basic.js
@@ -4,3 +4,20 @@ const navLinks = document.querySelector('.nav-links');
 menuToggle.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
+const headers = document.querySelectorAll('.accordion-header');
+
+headers.forEach(header => {
+  header.addEventListener('click', () => {
+    const body = header.nextElementSibling;
+    const icon = header.querySelector('.accordion-icon');
+
+    header.classList.toggle('active');
+    if (header.classList.contains('active')) {
+      body.style.maxHeight = body.scrollHeight + 'px';
+      icon.textContent = '-';
+    } else {
+      body.style.maxHeight = '0';
+      icon.textContent = '+';
+    }
+  });
+});
